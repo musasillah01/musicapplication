0ad048fc9f57b75670954f678867fe92ce55ef66
musasillah01 <sillahm550@gmail.com>
2022-10-29 08:07:43 +0000

    assignment 2 for zuri cohort

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # musicapplication
+
 this is my songcrud musicapp project
+
+i had issues with creating some of the relationships but i am still researching
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,31 @@
+from sys import maxsize
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    age = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Song(models.Model):
+    artist = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 50)
+    date_released = models.DateField() #default= datetime.today()
+    likes = models.PositiveIntegerField()
+    artiste_id = models.IntegerField(auto_created = False)  #auto_created = True, primary_key = True  
+    
+    def __str__(self):
+        return f"{self.title}"
+
+class Lyric(models.Model):
+    song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    
+    content = models.CharField(max_length = 1000)
+
+    #song_id = models.IntegerField()
+    
+
