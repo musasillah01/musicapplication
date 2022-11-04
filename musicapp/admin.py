04feb33706de6d841157c487e57e7b62304f5c33
musasillah01 <sillahm550@gmail.com>
2022-11-04 23:23:18 +0000

    New commit with relationship

--- musicapp/admin.py
+++ musicapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Artiste, Song, Lyric
+from .models import Artiste, Lyric, Song
 # Register your models here.
 
 admin.site.register(Artiste)
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,4 +1,4 @@
-from sys import maxsize
+
 from django.db import models
 from datetime import datetime
 # Create your models here.
@@ -13,19 +13,45 @@ class Artiste(models.Model):
 
 class Song(models.Model):
     artiste = models.ForeignKey(Artiste, on_delete = models.CASCADE)
-    title = models.CharField(max_length = 50)
+    title = models.CharField(max_length = 50, primary_key = True)
     date_released = models.DateField() #default= datetime.today()
     likes = models.PositiveIntegerField()
-    artiste_id = models.IntegerField(auto_created = False)  #auto_created = True, primary_key = True  
     
     def __str__(self):
         return f"{self.title}"
 
 class Lyric(models.Model):
-    song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
     
     content = models.CharField(max_length = 1000)
-
-    #song_id = models.IntegerField()
     
+    
+    
+    
+    
+'''
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    age = models.PositiveIntegerField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
 
+class Song(models.Model):
+    artiste = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+    title = models.CharField(max_length = 50)
+    date_released = models.DateField() #default= datetime.today()
+    likes = models.PositiveIntegerField()
+    # artiste_id = models.CharField()  #auto_created = True, primary_key = True  
+    
+    def __str__(self):
+        return f"{self.title}"
+
+class Lyric(models.Model):
+    # song = models.ForeignKey(Song, on_delete = models.CASCADE)
+    song_id = models.ForeignKey(Artiste, on_delete = models.CASCADE)
+    # song_id = models.CharField(max_length = 50)
+    content = models.CharField(max_length = 1000)
+    
+  '''  
